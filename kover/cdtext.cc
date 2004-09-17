18a39c1d61344b702da41fc1c2090456319a4c92
adrian <adrian>
2004-09-17 20:00:59 +0000

    do not print empty lines

--- kover/cdtext.cc
+++ kover/cdtext.cc
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: cdtext.cc,v 1.1 2004/09/17 18:23:36 adrian Exp $
+ * $Id: cdtext.cc,v 1.2 2004/09/17 20:00:59 adrian Exp $
  *
  * $Author: adrian $
  */
@@ -71,11 +71,15 @@ void cdtext::dump()
 	map < int, string >::iterator cur;
 
 	for (cur = track_name.begin(); cur != track_name.end(); cur++) {
+		if ((*cur).second == "")
+			continue;
 		cout << "track number " << (*cur).
 		    first << " has title " << (*cur).second << endl;
 	}
 	for (cur = track_performer.begin(); cur != track_performer.end();
 	    cur++) {
+		if ((*cur).second == "")
+			continue;
 		cout << "track number " << (*cur).
 		    first << " has performer " << (*cur).second << endl;
 	}
