75305765c9a06b8f7ac4ca4587dc12228454c964
Adrian Reber <adrian@lisas.de>
2008-01-07 14:46:39 +0100

    added license header

--- getcover/plugin.c
+++ getcover/plugin.c
@@ -1,3 +1,29 @@
+/*
+ * getcover - getcover is command-line tool to download covers
+ * Copyright (C) 2007 by Qball Cow <Qball@Sarine.nl>
+ * Copyright (C) 2007, 2008 by Adrian Reber
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * This code is largely based on the gmpc coveramazon plugin which
+ * is written by Qball Cow <Qball@Sarine.nl>.
+ *
+ * There are only minimal modifications from me (Adrian) to make it
+ * run as standalone command-line tool.
+ */
+
 #include <stdio.h>
 #include <string.h>
 #include <libxml/parser.h>
