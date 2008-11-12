3a6f68cd8a5c81d90876ceca49677e949bd33c00
Adrian Reber <adrian@lisas.de>
2008-11-12 21:50:09 +0100

    applied coding style with uncrustify

--- src/cddb_211_item.cc
+++ src/cddb_211_item.cc
@@ -1,81 +1,91 @@
-/** -*- adrian-c -*-
-	 kover - Kover is an easy to use WYSIWYG CD cover printer with CDDB support.
-	 Copyright (C) 2001-2003 by Adrian Reber 
-	 
-	 This program is free software; you can redistribute it and/or modify
-	 it under the terms of the GNU General Public License as published by
-	 the Free Software Foundation; either version 2 of the License, or
-	 (at your option) any later version.
-	 
-	 This program is distributed in the hope that it will be useful,
-	 but WITHOUT ANY WARRANTY; without even the implied warranty of
-	 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	 GNU General Public License for more details.
-	
-	 You should have received a copy of the GNU General Public License
-	 along with this program; if not, write to the Free Software
-	 Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-	 
-	 File: cddb_211_item.cc
-	 
-	 Description: wrapper class for the 211 match
-	 
-	 Changes:
-
-	 8 Jul 2001: Initial release
-
-*/
-
-/* $Id: cddb_211_item.cc,v 1.7 2005/06/25 19:43:24 adrian Exp $ */
+/*
+ * kover - Kover is an easy to use WYSIWYG CD cover printer with CDDB support.
+ * Copyright (C) 2001, 2008 by Adrian Reber
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
 
 #include "cddb_211_item.h"
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 
-cddb_211_item::cddb_211_item() {
-	 cddb_data = NULL;
+cddb_211_item::cddb_211_item()
+{
+	cddb_data = NULL;
 }
 
-cddb_211_item::cddb_211_item(char *input) {
-	 cddb_data = strdup(input);
+cddb_211_item::cddb_211_item(char *input)
+{
+	cddb_data = strdup(input);
 }
 
-cddb_211_item::cddb_211_item(string input) {
-	 cddb_data = strdup(input.c_str());
+cddb_211_item::cddb_211_item(string input)
+{
+	cddb_data = strdup(input.c_str());
 }
 
-cddb_211_item::~cddb_211_item() {
-	 if (cddb_data)
-		  free (cddb_data);
+cddb_211_item::~cddb_211_item()
+{
+	if (cddb_data)
+		free(cddb_data);
 }
 
-char *cddb_211_item::get() {
-	 return strdup(cddb_data);
+char *
+cddb_211_item::get()
+{
+	return strdup(cddb_data);
 }
 
- void cddb_211_item::set_title(string input) {
+void
+cddb_211_item::set_title(string input)
+{
 	title = input;
 }
-        void cddb_211_item::set_artist(string input){
+void
+cddb_211_item::set_artist(string input)
+{
 	artist = input;
 }
-        void cddb_211_item::set_category(string input){
-category = input;
+void
+cddb_211_item::set_category(string input)
+{
+	category = input;
 }
-        void cddb_211_item::set_id(unsigned long input){
-id = input;
+void
+cddb_211_item::set_id(unsigned long input)
+{
+	id = input;
 }
-        string cddb_211_item::get_title(){
-return title;
+string
+cddb_211_item::get_title()
+{
+	return title;
 }
-        string cddb_211_item::get_artist(){
-return artist;
+string
+cddb_211_item::get_artist()
+{
+	return artist;
 }
-        string cddb_211_item::get_category(){
-return category;
+string
+cddb_211_item::get_category()
+{
+	return category;
 }
-        unsigned long cddb_211_item::get_id(){
-return id;
+unsigned long
+cddb_211_item::get_id()
+{
+	return id;
 }
-
--- src/cddb_211_item.h
+++ src/cddb_211_item.h
@@ -1,32 +1,21 @@
-/** -*- adrian-c -*-
-	 kover - Kover is an easy to use WYSIWYG CD cover printer with CDDB support.
-	 Copyright (C) 2001 by Adrian Reber 
-	 
-	 This program is free software; you can redistribute it and/or modify
-	 it under the terms of the GNU General Public License as published by
-	 the Free Software Foundation; either version 2 of the License, or
-	 (at your option) any later version.
-	 
-	 This program is distributed in the hope that it will be useful,
-	 but WITHOUT ANY WARRANTY; without even the implied warranty of
-	 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	 GNU General Public License for more details.
-	 
-	 You should have received a copy of the GNU General Public License
-	 along with this program; if not, write to the Free Software
-	 Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-	 
-	 File: cddb_211_item.h 
-	 
-	 Description: cddb 211 items
-	 
-	 Changes:
-	 
-	 8 Jul 2001: Initial release
-	 
-*/
-
-/* $Id: cddb_211_item.h,v 1.4 2005/06/25 19:43:24 adrian Exp $ */
+/*
+ * kover - Kover is an easy to use WYSIWYG CD cover printer with CDDB support.
+ * Copyright (C) 2001, 2008 by Adrian Reber
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
 
 #ifndef CDDB_211_ITEM_H
 #define CDDB_211_ITEM_H
@@ -37,12 +26,12 @@
 using namespace std;
 
 class cddb_211_item {
-public:
-	 cddb_211_item();	 
-	 cddb_211_item(char *);
-	 cddb_211_item(string);
-	 ~cddb_211_item();
-	 char *get();
+ public:
+	cddb_211_item();
+	cddb_211_item(char *);
+	cddb_211_item(string);
+	~cddb_211_item();
+	char *get();
 	void set_title(string);
 	void set_artist(string);
 	void set_category(string);
@@ -51,8 +40,8 @@ public:
 	string get_artist();
 	string get_category();
 	unsigned long get_id();
-private:
-	 char *cddb_data;
+ private:
+	char *cddb_data;
 	string title;
 	string artist;
 	string category;
